b7b59b7d41438265a5126e1cca43e45ba9260f27
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-06 21:53:04 -0200

    Tuning the design

--- GoogleSheetsPOC/Buttons.swift
+++ GoogleSheetsPOC/Buttons.swift
@@ -4,6 +4,51 @@ import UIKit
 
 class CreateAccountButton: UIButton {
     
+    // MARK: Public interface
+    /// Corner radius of the background rectangle
+    public var roundRectCornerRadius: CGFloat = 2 {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    /// Color of the background rectangle
+    public var roundRectColor: UIColor = UIColor(hex: "8AD1DD") {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var keyValue: String {
+        get {
+            return self.titleLabel!.text!
+        }
+        set(value) {
+            self.setTitle(NSLocalizedString(value, comment: ""), for: .normal)
+        }
+    }
+    
+    // MARK: Overrides
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        commonInit()
+    }
+    
+    // MARK: Private
+    private var roundRectLayer: CAShapeLayer?
+    
+    private func commonInit() {
+        self.setTitleColor(UIColor.white, for: .normal)
+        
+        if let existingLayer = roundRectLayer {
+            existingLayer.removeFromSuperlayer()
+        }
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: roundRectCornerRadius).cgPath
+        shapeLayer.fillColor = roundRectColor.cgColor
+        self.layer.insertSublayer(shapeLayer, at: 0)
+        self.roundRectLayer = shapeLayer
+    }
 }
 
 // MARK: SignInButton subclass
