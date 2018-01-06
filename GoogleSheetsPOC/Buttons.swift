82c0e1f617d8ba9c9a228a3ae86baee2504919e8
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-06 20:32:08 -0200

    Adding authentication view, refactors and enhancements

---
+++ GoogleSheetsPOC/Buttons.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// MARK: CreateAccountButton subclass
+
+class CreateAccountButton: UIButton {
+    
+}
+
+// MARK: SignInButton subclass
+
+class SignInButton: UIButton {
+    
+}
---
+++ GoogleSheetsPOC/Labels.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+// MARK: BasicLabel subclass
+
+class BasicLabel: UILabel {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    func commonInit() {
+        self.textColor = UIColor.white
+    }
+    
+    @IBInspectable var keyValue: String {
+        get {
+            return self.text!
+        }
+        set(value) {
+            self.text = NSLocalizedString(value, comment: "")
+        }
+    }
+}
+
+// MARK: SubtitleLabel subclass
+
+class SubtitleLabel: UILabel {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    func commonInit() {
+        self.textColor = UIColor.white
+        self.font.withSize(20)
+    }
+    
+    @IBInspectable var keyValue: String {
+        get {
+            return self.text!
+        }
+        set(value) {
+            self.text = NSLocalizedString(value, comment: "")
+        }
+    }
+}
