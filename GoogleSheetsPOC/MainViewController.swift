98a83c173467d0c61837095171e6aa3b3be8c386
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-10 23:44:48 -0200

    Adding new class for main view controller

---
+++ GoogleSheetsPOC/MainViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBAction func createAccountButton(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+    }
+    
+}
