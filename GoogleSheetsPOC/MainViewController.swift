a42bd8350f2472137bc7da2a3db5d4ca5a4cd664
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-13 12:56:01 -0200

    View adjustments

--- GoogleSheetsPOC/MainViewController.swift
+++ GoogleSheetsPOC/MainViewController.swift
@@ -3,7 +3,8 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBAction func createAccountButton(_ sender: Any) {
-        
+        let viewController = SignInViewController(nibName: "SignInViewController", bundle: nil)
+        present(viewController, animated: true) {() -> Void in }
     }
     
     override func viewDidLoad() {
