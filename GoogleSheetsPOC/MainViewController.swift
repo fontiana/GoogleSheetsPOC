f08360e2c7944db2603c0a59c4281f93754a7a4b
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-15 23:28:54 -0200

    Adjustments and tests

--- GoogleSheetsPOC/MainViewController.swift
+++ GoogleSheetsPOC/MainViewController.swift
@@ -1,14 +1,49 @@
+import GoogleAPIClientForREST
+import GoogleSignIn
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+    
+    private let scopes = [kGTLRAuthScopeSheetsSpreadsheetsReadonly]
+    private let service = GTLRSheetsService()
     
     @IBAction func createAccountButton(_ sender: Any) {
-        let viewController = SignInViewController(nibName: "SignInViewController", bundle: nil)
-        //TODO: Add transition effect https://stackoverflow.com/questions/41440934/swift-unable-to-present-view-controller-from-top-to-bottom
-        self.present(viewController, animated: true) {() -> Void in }
+        GIDSignIn.sharedInstance().signIn()
     }
+    
+    // MARK: Overrides
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = [
+            "https://www.googleapis.com/auth/spreadsheets"
+        ]
+        
+        GIDSignIn.sharedInstance().clientID = "832262025030-be31sijfsd1btmfh6f3hm0u0bd8u1nou.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
     }
     
+    // MARK: Private functions
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            let alertController = UIAlertController(title: "Authentication Error",
+                                                    message: error.localizedDescription, preferredStyle: .alert)
+            alertController.show()
+            self.service.authorizer = nil
+        } else {
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            navigate()
+        }
+    }
+    
+    func navigate() {
+        let setSheetViewController = SetSheetViewController()
+        setSheetViewController.service = service
+        self.present(setSheetViewController, animated: true, completion: nil)
+    }
 }
