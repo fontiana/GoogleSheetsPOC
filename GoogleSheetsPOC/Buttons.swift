c4a6412ab551b226b9524f25518e33d485ae02e3
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-01-16 23:57:08 -0200

    adding image to button

--- GoogleSheetsPOC/Buttons.swift
+++ GoogleSheetsPOC/Buttons.swift
@@ -1,57 +1,5 @@
 import UIKit
 
-// MARK: CreateAccountButton subclass
-
-class CreateAccountButton: UIButton {
-    
-    // MARK: Public interface
-    /// Corner radius of the background rectangle
-    public var roundRectCornerRadius: CGFloat = 3 {
-        didSet {
-            self.setNeedsLayout()
-        }
-    }
-    
-    /// Color of the background rectangle
-    public var roundRectColor: UIColor = UIColor(hex: "8AD1DD") {
-        didSet {
-            self.setNeedsLayout()
-        }
-    }
-    
-    @IBInspectable var keyValue: String {
-        get {
-            return self.titleLabel!.text!
-        }
-        set(value) {
-            self.setTitle(NSLocalizedString(value, comment: ""), for: .normal)
-        }
-    }
-    
-    // MARK: Overrides
-    override public func layoutSubviews() {
-        super.layoutSubviews()
-        commonInit()
-    }
-    
-    // MARK: Private
-    private var roundRectLayer: CAShapeLayer?
-    
-    private func commonInit() {
-        self.setTitleColor(UIColor.white, for: .normal)
-        self.contentEdgeInsets = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
-        
-        if let existingLayer = roundRectLayer {
-            existingLayer.removeFromSuperlayer()
-        }
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = UIBezierPath(roundedRect: self.bounds, cornerRadius: roundRectCornerRadius).cgPath
-        shapeLayer.fillColor = roundRectColor.cgColor
-        self.layer.insertSublayer(shapeLayer, at: 0)
-        self.roundRectLayer = shapeLayer
-    }
-}
-
 // MARK: SignInButton subclass
 
 class SignInButton: UIButton {
@@ -74,6 +22,7 @@ class SignInButton: UIButton {
     // MARK: Private
     private func commonInit() {
         self.setTitleColor(UIColor.white, for: .normal)
+        self.setImage(UIImage(named: "google-logo.png"), for: .normal)
         self.contentEdgeInsets = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
         self.layer.borderWidth = 1 //TODO: Change to image so there will be fade-out animation
         self.layer.borderColor = UIColor.white.cgColor //TODO: Change to image so there will be fade-out animation
