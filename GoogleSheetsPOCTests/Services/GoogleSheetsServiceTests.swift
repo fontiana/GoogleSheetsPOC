046b5b135ae16bad9f235be7f31e28c6dcda064b
Victor Oliveira <victoroliveira@Victors-MacBook-Pro.local>
2018-03-11 21:21:24 -0300

    Adding unit tests

---
+++ GoogleSheetsPOCTests/Services/GoogleSheetsServiceTests.swift
@@ -0,0 +1,24 @@
+//
+//  GoogleSheetsPOCTests.swift
+//  GoogleSheetsPOCTests
+//
+//  Created by Victor Oliveira on 3/11/18.
+//  Copyright © 2018 Fonts. All rights reserved.
+//
+
+import XCTest
+@testable import GoogleSheetsPOC
+
+class GoogleSheetsServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testGetSpreadsheet() {
+    }
+}
